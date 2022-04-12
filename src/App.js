6566e82e27fdcaf04d79789358230dd344cec6df
shamsuladd07 <shamsuladd07@gmail.com>
2022-04-12 22:16:23 +0600

    sign up and login completed with react firebase hooks

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import About from './components/About/About';
 import Header from './components/Header/Header';
 import Shop from './components/Header/Shop/Shop';
 import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
 import Orders from './components/Orders/Orders';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './SignUp.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
+
+const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value);
+    }
+
+    if (user) {
+        navigate('/shop');
+    }
+
+    const handleCreateUser = event => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setError('Your tow password did not match')
+            return;
+        }
+        if (password.length < 6) {
+            setError('password must be 6 characters or longer')
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password);
+    }
+
+    return (
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form onSubmit={handleCreateUser}>
+                    <div className="input-group">
+                        <label className='email' htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label className='password' htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label className='password' htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <div className='new-join'>
+                    <p className='new-join-title'>Already have an account? <Link className='form-link' to="/login"> Login</Link></p>
+                </div>
+                <div >
+                    <div className='hr-container'>
+                        <hr />
+                        or
+                        <hr />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
